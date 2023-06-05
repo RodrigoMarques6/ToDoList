92bc9a708d820b0b8b860c8862db29686eb53708
RodrigoMarques <rodrigomarques328@hotmail.com>
2023-06-05 16:58:13 -0300

    todolist

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,1 +1,11 @@
-//teste
+// A camada de controllers vai salvar as funções que foram passadas no router
+
+const getAll = (req, res) => {
+    return res.status(200).json({message: 'Controller está correto'});
+};
+
+module.exports = {
+    getAll
+}
+
+// Agora vamos importar essa função lá no router
--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 // Criamos um arquivo só para armazenar as rotas, de forma que a aplicação fique organizada
 
 const express = require('express');
+const tasksController = require('./controllers/tasksController');
 
 const router = express.Router();
 
 module.exports = router;
 
 
-router.get('/tasks')
+router.get('/tasks', tasksController.getAll)
 // Nós iríamos criar as rotas agora, porém, para organizar a aplicação, criaremos a pasta controller para facilitar
+
+// Nesse caso, o tasksController é um controller que está sendo executado após o endpoint "tasks" e após vai executar o getAll que está em tasksController, que por sua vez executará o que foi passado pra função getAll
