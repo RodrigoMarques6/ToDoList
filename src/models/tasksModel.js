b6e77374b8730400053c029466f3f6aba2db5f86
RodrigoMarques <rodrigomarques328@hotmail.com>
2023-06-06 17:40:34 -0300

    todolist

--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -10,7 +10,14 @@ const getAll = async () => {
 };
 
 // Função para cadastrar uma nova tarefa no banco:
+const createTask = async (task) => {
+    // Abaixo nós vamos receber somente o título da task, então estamos passando o "title".
+    const { title } = task;
 
+    // Abaixo vamos escrever algo para inserir no banco:
+    const createTask = await connection.execute('INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)', [1, 2, 3])
+    // As interrogações são a quantidade de informações, ou seja, cada uma recebe uma informação. Os valores do array são para cada interrogação.
+}
 
 module.exports = {
     getAll
