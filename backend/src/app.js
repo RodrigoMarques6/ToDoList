6ddabfcbf353707a2fd10e6dd8650680f8f8d828
RodrigoMarques <rodrigomarques328@hotmail.com>
2023-06-12 16:08:26 -0300

    todolist

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// Vamos importar o cors para dar acesso ao client:
+const cors = require('cors');
+const router = require('./router');
+
+const app = express();
+
+app.use(express.json())
+// O código acima: Habilitar a utilização do json através do express
+app.use(router);
+// Acima explica que o nosso app vai sempre utilizar o nosso router
+
+// Vamos colocar para o cors ser utilizado pela aplicação:
+app.use(cors)
+
+module.exports = app;
