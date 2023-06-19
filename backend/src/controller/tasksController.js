4492416b8e2dfa8052bce7ef00bcd30df1629eb7
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-06-19 11:12:53 -0300

    todolist

--- backend/src/controller/tasksController.js
+++ backend/src/controller/tasksController.js
@@ -4,13 +4,14 @@ const tasksModel = require('../models/tasksModel');
 
 // Abaixo a função que retorna algo no front:
 
-const getAll = (req, res) => {
+const getAll = async (req, res) => {
 
-    // Adicionando uma função chamada tasks para armazenar o retorno da função getAll do tasksController importada para esse arquivo
+    // Adicionando uma função chamada tasks para armazenar o retorno da função getAll do tasksController importada para esse arquivo;
+    // Tendo em vista a função importada ser assíncrona, ela leva um tempinho para retornar os dados.
 
-    const tasks = tasksModel.getAll();
+    const tasks = await tasksModel.getAll();
 
-    return res.status(200).json( {message: 'Controller está ok'})
+    return res.status(200).json(tasks)
 };
 
 // Exportanto a função que retorna algo no front para ser utilizado publicamente no arquivo de código:
@@ -18,3 +19,5 @@ const getAll = (req, res) => {
 module.exports = {
     getAll
 };
+
+// Em resumo: A função getAll que é a midleware que está sendo chamada no router vai utilizar a função getAll da model para trazer os dados do banco através da tasksModel, armazenar a const tasks e vai enviar(retornar) as tasks como resposta lá no front através do "return(tasks)"
