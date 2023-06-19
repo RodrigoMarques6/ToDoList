79db0912fd9b2b09b80c91167e4d2612fb4319ad
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-06-19 10:18:27 -0300

    todolist

--- backend/src/controller/tasksController.js
+++ backend/src/controller/tasksController.js
@@ -1,6 +1,19 @@
+// Vamos importar toda a taskModel para ser utilizada aqui e conseguir retornar um array lá no front:
+
+const tasksModel = require('../models/tasksModel');
+
+// Abaixo a função que retorna algo no front:
+
 const getAll = (req, res) => {
+
+    // Adicionando uma função chamada tasks para armazenar o retorno da função getAll do tasksController importada para esse arquivo
+
+    const tasks = tasksModel.getAll();
+
     return res.status(200).json( {message: 'Controller está ok'})
 };
+
+// Exportanto a função que retorna algo no front para ser utilizado publicamente no arquivo de código:
 
 module.exports = {
     getAll
