40b529fec494dd31a17853b6efc2d4cd5145bc4b
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-06-26 13:53:20 -0300

    todolist

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,7 +10,7 @@ const tasksController = require('./controller/tasksController')
 
 // Rota para retornar um array com os itens do banco de dados:
 router.get('/tasks', tasksController.getAll);
-router.get('/tasks2', (req, res) => res.send('Olá mundo2'))
+router.get('/tasks2', (req, res) => res.send('Estou na rota tasks2'));
 module.exports = router;
 
 // A pasta models armazena todas as funções que vão se conectar diretamente com o banco de dados
