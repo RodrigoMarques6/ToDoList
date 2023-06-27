30db281c6ef65b6133c9bcd4186d16c32b1f2588
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-06-26 21:28:01 -0300

    todolist

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+// Após finalizar nosso backend, vamos começar o frontend, mas primeiro importando o cors com "npm install cors" e requerendo o cors no código conforme abaixo:
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
+
+// Vamos liberar o cors para todos utilizarem: 
+app.use(cors());
 
 // Função para a API trabalhar com dados em json
 app.use(express.json());
