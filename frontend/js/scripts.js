44b896ff98486aeb70bf6c099f96c13dbd615bc5
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-06-29 16:47:25 -0300

    todolist

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,6 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Primeiro de tudo: DOM funcionando perfeitamente!')
+});
+
 const tbody = document.querySelector('tbody');
 const addForm = document.querySelector('.add-form');
 const inputTask = document.querySelector('.input-task');
+
 
 // Função para buscar as tasks no servidor (lembrar que é sempre uma função assíncrona, tendo em vista não sabermos quanto tempo vai levar para realizar a busca)
 
@@ -29,10 +34,45 @@ const addTask = async (event) => {
     });
      // Adicionamos após o endpoint o método da requisição para que não seja utilizado o padrão que é o get.
 
+     // Recarregar a página após adição de uma task (não aparece na tela se não der um refresh)
      loadTasks();
+     
+     inputTask.value = '';
+     console.log(`Adicionamos uma task com sucesso!`)
 }
 
+// Função para excluir uma task:
 
+const deleteTask = async (id) => {
+    await fetch(`http://localhost:3333/tasks/${id}`, {
+        method: 'delete',
+    });
+
+    loadTasks();
+    console.log(`Excluimos a task de ID:${id} com sucesso!`)
+}
+
+// Função para atualizar tarefas e conseguir tratar a parte do select:
+
+const updateTask = async (task) => {
+    // Passamos uma task inteira, pois vamos atualizá-la completamente
+    const { id, title, created_at, status} = task;
+    await fetch(`http://localhost:3333/tasks/${id}`, {
+        method: 'put',
+        headers: {'Content-type': 'application/json' },
+        body: JSON.stringify({ title, status }),
+    });
+
+    loadTasks();
+}
+
+// Função para formatar a data que aparece nas tasks:
+
+const formatDate = (dateUTC) => {
+    const options = {dateStyle: 'long', timeStyle: 'short'}
+    const date = new Date(dateUTC).toLocaleString(options);
+    return date;
+};
 
 // Função que cria elementos HTML:
 const createElement = (tag, innerText = '', innerHTML = '') => {
@@ -77,7 +117,7 @@ const createRow = (task) => {
     const tdTitle = createElement('td', title);
 
     // Para criarmos todos os "td", usaríamos "tdTitle.innerText = title;" por exemplo. Como são muitos, vamos utilizar uma forma diferente. Vamos na função de criar elemento (createElement) e vamos passar um parâmetro nela denominado "innerText". Trata-se de um parâmetro opcional. A mesma coisa ocorre para o created_at abaixo.
-    const tdCreatedAt = createElement('td', created_at); 
+    const tdCreatedAt = createElement('td', formatDate(created_at)); 
 
     // Agora precisamos além de criar um "td", precisamos criar um select e também option. No caso, são 3 selects e 3 options.
     // Vamos criar primeiro o td
@@ -89,16 +129,48 @@ const createRow = (task) => {
 
     const select = createSelect(status);
 
+    // Vamos adicionar um evento escutador no nosso select para quando eu modificar o status da task:
+
+    select.addEventListener('change', ({target}) => (updateTask({ id, title, created_at, status: target.value})));
+
     // Vamos criar o elemento botão:
     // Passamos o span do botão:
     const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
 
     const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
 
+    // Adicionando um pequeno form para edição do campo da task:
+
+    const editForm = createElement('form');
+    const editInput = createElement('input');
+
+    editInput.value = title;
+    editForm.appendChild(editInput);
+
+    // Vamos permitir que, após edição do campo de task, possa realizar o envio:
+
+    editForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        updateTask({ id, title: editInput.value, status })
+    })
+
+    editButton.addEventListener('click', () => {
+        tdTitle.innerText = '';
+        tdTitle.appendChild(editForm);
+    })
+    
     // Adicionando a classe dos botões aqui no JS:
 
     editButton.classList.add('btn-action');
     deleteButton.classList.add('btn-action');
+
+    // Adicionando funcionalidade ao botão de excluir que está sendo criado dinamicamente com o JS:
+
+    deleteButton.addEventListener('click', () => {
+        deleteTask(id);
+    })
+
 
     // Vamos tratar o select abaixo:
 
@@ -124,6 +196,10 @@ const createRow = (task) => {
 const loadTasks = async () => {
     // Utilizando a função que já existe no código para buscar as tasks no banco:
     const tasks = await fetchTasks();
+
+    // Vamos limpar o HTML antes de realizar a adição de tasks na tela:
+
+    tbody.innerHTML = '';
 
     // Precisamos percorrer o array de tarefas e montar as tarefas pra exibir na tela:
     tasks.forEach((task) => {
